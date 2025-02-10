0eb92cfd7cc1d266c98148649ca51095fe5cd44f
nurulislamrimon <nurulislamrimon@gmail.com>
2025-02-10 22:57:39 +0600

    validation added

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,6 +1,16 @@
+import { IsInt, IsString, IsEmail, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
   name: string;
+
+  @IsInt()
   age: number;
+
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @MinLength(6)
   password: string;
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,6 +6,8 @@ import {
   Patch,
   Param,
   Delete,
+  ValidationPipe,
+  UsePipes,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
@@ -16,6 +18,7 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
+  @UsePipes(new ValidationPipe())
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
