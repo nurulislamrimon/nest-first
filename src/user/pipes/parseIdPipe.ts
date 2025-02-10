e46c134a0bfc6f9f9f76e1b998a15e4f9cacfd4f
nurulislamrimon <nurulislamrimon@gmail.com>
2025-02-11 00:06:44 +0600

    injectable added for external use

--- src/user/pipes/parseIdPipe.ts
+++ src/user/pipes/parseIdPipe.ts
@@ -1,11 +1,8 @@
-import {
-  ArgumentMetadata,
-  BadRequestException,
-  PipeTransform,
-} from '@nestjs/common';
+import { BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
 
+@Injectable()
 export class ParseIdPipe implements PipeTransform<string, number> {
-  transform(value: string, metadata: ArgumentMetadata): number {
+  transform(value: string): number {
     const id = parseInt(value, 10);
     if (isNaN(id)) {
       throw new BadRequestException('id Must be a number');
